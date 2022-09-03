4da0c7b83ef6aee9758f0a150124a5450d80439d
Jesse Youngblood <jessetyoungblood@gmail.com>
2022-09-03 19:46:53 -0400

    Add unit test for config function

---
+++ tests/config.test.ts
@@ -0,0 +1,33 @@
+jest.mock('../src/load');
+import { mocked } from 'jest-mock';
+import { config } from '../src/index';
+import { load } from '../src/load'
+
+const loadMocked = mocked(load, true)
+
+describe('config() function', () => {
+
+  test(`properly returns typed env`, () => {
+    loadMocked.mockReturnValue({
+      PORT: 8080,
+      HOST: 'api.example.com',
+      VERBOSE: true
+    });
+
+    const typed = config<{
+      host: string;
+      port: number;
+      verbose: boolean
+    }>((env) => {
+      return {
+        host: env.HOST,
+        port: env.PORT,
+        verbose: env.VERBOSE
+      };
+    });
+
+    expect(typed.host).toBe('api.example.com');
+    expect(typed.port).toBe(8080);
+    expect(typed.verbose).toBe(true);
+  });
+});
